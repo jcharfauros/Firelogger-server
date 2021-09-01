744f7e5815bb0a27b94c1c1e24c136276699f2a4
Marcus Elliott <webdevhero2010@gmail.com>
2021-08-31 20:20:10 -0400

    2nd Push corrections

--- db.js
+++ db.js
@@ -1,16 +1,1 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("firelogger", "postgres", "password", {
-  host: "localhost",
-  dialect: "postgres",
-});
-
-sequelize.authenticate().then(
-  function () {
-    console.log("Connected to FireLogger postgres database");
-  },
-  function (err) {
-    console.log(err);
-  }
-);
-
-module.exports = sequelize;
+/* delete me */
