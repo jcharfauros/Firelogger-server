7cb964b40a10911ec1b28abe68bf94b641ca7e6b
Stephanie Falls <stephms@gmail.com>
2021-08-31 20:32:05 -0400

    stephanie correction

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-require("dotenv").config();
-let express = require('express');
-let app = express();
-
-app.listen(3000, function() {
-    console.log("App is listening on port 3000");
-});
+/* delete me */
