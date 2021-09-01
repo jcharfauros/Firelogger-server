1c97e16d8f60da6b794bf993748584ebeab4d973
jcharfauros <julia.charfauros@gmail.com>
2021-08-31 20:23:32 -0400

    Added code to app.js, db.js, headers.js, validate-session.js. Added .env file

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-/* delete me */
-/* delete me */
+require('dotenv').config();
+let express = require('express');
+let app = express();
+let sequelize = require('./db');
+
+let user = require('./controllers/userController');
+let inventory = require('./controllers/inventoryController');
+
+sequelize.sync();
+
+app.use(express.json());
+
+app.use('/user', user); //no validation route, will probably not use - jdc
+app.use(require('./middleware/validate-session'));
+app.use('/inventory', inventory);
+
+app.listen(3000, function() {
+    console.log('App is listening intently on port 3000');
+})
--- db.js
+++ db.js
@@ -1,1 +1,21 @@
-/* delete me */
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize (
+    'firelogger',
+    'postgres',
+    'password', 
+    {
+        host: 'localhost',
+        dialect: 'postgres'
+    }
+);
+
+sequelize.authenticate().then(
+    function() {
+        console.log('Woot, connected to firelogger postgres database!');
+    },
+    function(err) {
+        console.log(err);
+    }
+);
+
+module.exports = sequelize;
