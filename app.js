8d4e146711d8e8c0c0c76c0ee44baf1f9149567f
lesaphil1150 <lesaphil1150@gmail.com>
2021-09-13 10:05:12 -0400

    Leslie pushing

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 let express = require("express");
 let app = express();
 let sequelize = require("./db");
+let fetch = require("node-fetch"); //NEW
 
 let user = require("./controllers/userController");
 let inventory = require("./controllers/inventoryController");
+const { response, request } = require("express");
 
 sequelize.sync();
 
@@ -13,6 +15,26 @@ app.use(require("./middleware/headers"));
 
 app.use("/user", user); //no validation route, will probably not use - jdc
 app.use("/inventory", inventory);
+
+// ADDING NEW ENDPOINT FOR HOTEL FETCH BELOW
+app.get("/hotels", async (req, res) => {
+  let apiKey = "AIzaSyBmjCLBY3Fk2Yn5J6997aEO2_x4_9mYuQk";
+  let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?radius=5000&language=en&query=hotel&key=${apiKey}`;
+
+  let response = await fetch(url);
+  let data = await response.json();
+  res.json(data);
+});
+
+// ADDING NEW ENDPOINT FOR PET LODGING FETCH BELOW
+app.get("/petcare", async (req, res) => {
+  let apiKey = "AIzaSyBmjCLBY3Fk2Yn5J6997aEO2_x4_9mYuQk";
+  let url = `https://maps.googleapis.com/maps/api/place/textsearch/json?radius=5000&language=en&query=petlodging&key=${apiKey}`;
+
+  let response = await fetch(url);
+  let data = await response.json();
+  res.json(data);
+});
 
 app.listen(3000, function () {
   console.log("App is listening intently on port 3000");
